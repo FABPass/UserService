2cf385d9a535c49f5a3441d337518b7b94058617
Belmin Selimovic <bselimovic1@etf.unsa.ba>
2022-04-01 15:20:55 +0200

    Config client configuration with actuator

--- src/main/java/com/example/userservice/UserServiceApplication.java
+++ src/main/java/com/example/userservice/UserServiceApplication.java
@@ -1,16 +1,31 @@
 package com.example.userservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RestController
+@RefreshScope
 public class UserServiceApplication {
+
+    @Value("${message}")
+    private String message;
 
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
     }
+
+    @GetMapping
+    public String hello(){
+        return message;
+    }
+
 
 
 }
